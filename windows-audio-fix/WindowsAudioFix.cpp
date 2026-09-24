24dc9f21fd9314d75fb74f41a85890c642a6bf22
Rodolfo Martinez-Maldonado <mandm.rodolfo@gmail.com>
2026-09-24 17:35:53 -0600

    Working on getting Windows Service status with dynamic memory

--- windows-audio-fix/WindowsAudioFix.cpp
+++ windows-audio-fix/WindowsAudioFix.cpp
@@ -12,11 +12,25 @@ This script will restart the service and fix the issue.
 #include <winsvc.h>
 #include <iostream>
 
+#define messageSize 512
+
+void FormatMessageWrapper(DWORD lastErrorResult, LPWSTR messageBuffer, DWORD bufferSize){
+    DWORD check = FormatMessageW(
+        FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, // FORMAT_MESSAGE_ALLOCATE_BUFFER
+        NULL,
+        lastErrorResult,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        messageBuffer,
+        bufferSize,
+        NULL);
+
+    // If message formatting failed
+    if (!check){
+        //
+    }
+}
 
 int main(){
-    //
-    // BOOL temp = QueryServiceObjectSecurity();
-
     // Try opening the SCM
     SC_HANDLE hSCM = OpenSCManagerW(NULL, NULL, SC_MANAGER_ALL_ACCESS);
 
@@ -70,6 +84,56 @@ int main(){
 
     MessageBoxW(NULL, L"Was able to open Audio", L"Success", MB_OK);
 
+    // Get the required amount of buffer first
+    // so that we can allocate necessary memory
+    // As maximum required buffer for QueryServiceStatusEx is 8k bytes
+    DWORD getRequiredLength = 0;
+    BOOL getStatusLengthBool = QueryServiceStatusEx(windowsService, SC_STATUS_PROCESS_INFO, NULL, 0, &getRequiredLength);
+
+    DWORD buffer_error = GetLastError();
+    // GetLastError should return ERROR_INSUFFICIENT_BUFFER
+    // If not, something went wrong so terminate early
+    if (buffer_error != ERROR_INSUFFICIENT_BUFFER){
+        MessageBoxW(NULL, L"Required buffer size was not returned", L"Error", MB_OK);
+        wchar_t lpMsg[512] = { 0 };
+
+        DWORD e = FormatMessageW(
+            FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS, // FORMAT_MESSAGE_ALLOCATE_BUFFER
+            NULL,
+            buffer_error,
+            MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+            lpMsg,
+            sizeof(lpMsg)/sizeof(wchar_t),
+            NULL);
+        
+        if (!e){
+            MessageBoxW(NULL, L"Couldn't receive message", L"Error", MB_OK);
+        } else {
+            MessageBoxW(NULL, lpMsg, L"Error", MB_OK);
+        }
+
+        CloseServiceHandle(windowsService);
+        CloseServiceHandle(hSCM);
+        return 0;
+    }
+
+    std::cout << "Required buffer is " << getRequiredLength << std::endl;
+
+    // Now get the SERVICE_STATUS_PROCESS struct for Windows Audio
+    //BYTE audioStatus[getRequiredLength] = { 0 };
+    BYTE* audioStatus = new (std::nothrow) BYTE[getRequiredLength];
+
+    if (audioStatus == nullptr){
+        MessageBoxW(NULL, L"Dynamic Allocation for Windows Audio Status failed", L"Error", MB_OK);
+        CloseServiceHandle(windowsService);
+        CloseServiceHandle(hSCM);
+        return 0;
+    }
+
+    BOOL audioStatusBool = QueryServiceStatusEx(windowsService, SC_STATUS_PROCESS_INFO, audioStatus, getRequiredLength, NULL);
+
+    delete[] audioStatus;
+    audioStatus = nullptr;
     // Close the handles for both the Windows Audio and SCM
     CloseServiceHandle(windowsService);
     CloseServiceHandle(hSCM);
